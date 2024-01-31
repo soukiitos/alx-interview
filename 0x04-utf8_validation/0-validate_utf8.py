a75246437700d9f353b806de8b58d44285511f27
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-01 00:38:14 +0100

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,18 +10,18 @@ def validUTF8(data):
     num = 0
     for b in data:
         if num == 0:
-            if b >> 5 == 0b110:
-                num = 1
-            elif b >> 4 == 0b1110:
-                num = 2
-            elif b >> 3 == 0b11110:
-                num = 3
-            elif b >> 7 == 0b0:
+            if (b >> 7) == 0b0:
                 num = 0
+            elif (b >> 5) == 0b110:
+                num = 1
+            elif (b >> 4) == 0b1110:
+                num = 2
+            elif (b >> 3) == 0b11110:
+                num = 3
             else:
                 return False
         else:
-            if b >> 6 != 0b10:
+            if (b >> 6) != 0b10:
                 return False
             num -= 1
     return num == 0

342ce83978859e94e0a152551d5064ae8e068b4f
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-01 01:38:03 +0100

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,20 +8,20 @@ represents a valid UTF-8 encoding
 def validUTF8(data):
     '''Define validUTF8'''
     num = 0
+    byte1 = 1 << 7
+    byte2 = 1 << 6
     for b in data:
         if num == 0:
-            if (b >> 7) == 0b0:
-                num = 0
-            elif (b >> 7) == 0b110:
-                num = 1
-            elif (b >> 7) == 0b1110:
-                num = 2
-            elif (b >> 7) == 0b11110:
-                num = 3
-            else:
+            i = 1 << 7
+            while i & b:
+                num += 1
+                i = i >> 1
+            if num == 0:
+                continue
+            if num == 1 or num > 4:
                 return False
         else:
-            if (b >> 6) != 0b10:
+            if not (b & byte1 and not (b & byte2)):
                 return False
-            num -= 1
+        num -= 1
     return num == 0

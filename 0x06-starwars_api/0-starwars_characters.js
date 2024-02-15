d2c43862396075bac0abcb3d160183aa739b167e
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-16 00:54:38 +0100

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = (array, a) => {
+  if (a === array.length) return;
+  request(array[a], (_error, response, body) => {
+    if (_error) {
+      throw error;
+    } else {
+        console.log(JSON.parse(body).name);
+        movieId(array, a + 1);
+    }
+  });
+};
+
+request(
+  `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
+  (_error, response, body) => {
+    if (_error) {
+      throw _error;
+    } else {
+      const charachtersReqBody = JSON.parse(body).characters;
+      movieId(charachtersReqBody, 0);
+    }
+  }
+);

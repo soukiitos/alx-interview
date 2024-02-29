755c85a0dec33afe30845f6f15be732828384023
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-29 17:19:47 +0100

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,12 +6,15 @@ def makeChange(coins, total):
     """MakeChange"""
     if total <= 0:
         return 0
-    min_coin = [float('inf')] * (total + 1)
-    min_coin[0] = 0
-    for coin in coins:
-        for tot_coins in range(coin, total + 1):
-            min_coin[tot_coins] = min(
-                    min_coin[tot_coins],
-                    min_coin[tot_coins - coin] + 1
-                    )
-    return min_coin[total] if min_coin[total] != float('inf') else -1
+    coins.sort(reverse=True)
+    count_coin = 0
+    i_coins = 0
+    while total > 0:
+        if i_coins >= len(coins):
+            return -1
+        if total - coins[i_coins] >= 0:
+            total -= coins[i_coins]
+            count_coin += 1
+        else:
+            i_coins += 1
+    return count_coin

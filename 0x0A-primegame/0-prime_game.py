3a02a7a4070b9b061926581ca9df2c46c7c10e55
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-03-13 15:59:53 +0000

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,19 +4,26 @@
 
 def isWinner(x, nums):
     """Define is winer function"""
-    def is_winner_of_game(n):
-        """Define the winner of game"""
-        if n < 2:
-            return "Ben"
+    def is_prime(num):
+        """Define is prime"""
+        if num < 2:
+            return False
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % 1 == 0:
+                return False
+        return True
+
+    def get_prime(n):
+        '''Define get prime'''
         primes = []
         for num in range(2, n + 1):
-            is_prime = True
-            for i in range(2, int(num ** 0.5) + 1):
-                if num % i == 0:
-                    is_prime = False
-                    break
-            if is_prime:
+            if is_prime(num):
                 primes.append(num)
+        return primes
+
+    def winner_game(n):
+        '''Define winner game'''
+        primes = get_prime(n)
         if len(primes) % 2 == 0:
             return "Ben"
         else:
@@ -24,9 +31,8 @@ def isWinner(x, nums):
 
     win = {"Maria": 0, "Ben": 0}
     for n in nums:
-        winner = is_winner_of_game(n)
+        winner = winner_game(n)
         win[winner] += 1
-
     max_win = max(win.values())
     if win["Maria"] == win["Ben"]:
         return None
